fe73b72c0a37c0bdb8537c11a486881bf044ba48
김규민 <abc2232002@gmail.com>
2025-11-23 14:23:25 +0900

    feat: 마커 하나 넣는 기능 추가함.

--- app/results/GoogleMap.tsx
+++ app/results/GoogleMap.tsx
@@ -10,40 +10,52 @@ declare global {
   }
 }
 
+type Marker = {
+  lat: number;
+  lng: number;
+  title: string;
+};
+
 interface GoogleMapProps {
   center: { lat: number; lng: number };
   level: number;
+  ㅡarkers: Marker[];
 }
 
-function GoogleMap({ center, level = 3 }: GoogleMapProps) {
+function GoogleMap({ center, level = 3, markers }: GoogleMapProps) {
   const container = useRef(null);
   const [isLoading, setIsLoading] = useState(true);
-  const init = () => {
+  const init = async () => {
     setIsLoading(false);
-    window.kakao.maps.load(() => {
-      const options = {
-        center: new window.kakao.maps.LatLng(center.lat, center.lng), //지도의 중심좌표.
-        level: level, //지도의 레벨(확대, 축소 정도)
-      };
+    if (!container.current) return;
 
-      const map = new window.kakao.maps.Map(container.current, options);
-    });
+    const [{ Map }, { AdvancedMarkerElement }] = await Promise.all([
+      window.google.maps.importLibrary("maps") as Promise<google.maps.MapsLibrary>,
+      window.google.maps.importLibrary("marker") as Promise<google.maps.MarkerLibrary>,
+    ]);
+
+    const innerMap = container.current.innerMap;
+    innerMap.setOptions({ mapTypeControl: false });
+
+    const makeMarker = (marker: Marker, innerMap) =>
+      new AdvancedMarkerElement({
+        map: innerMap,
+        position: { lat: marker.lat, lng: marker.lng },
+        title: marker.title,
+      });
+
+    makeMarker({ lat: container.current.center.lat(), lng: container.current.center.lng(), title: "aaa" }, innerMap);
   };
 
   return (
     <div className="w-full">
-      {/* <Script
-        async
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=05ae95d1e0cb9dc4e1ddc052ccc303a5&autoload=false`}
-        onLoad={init}
-      />
-      <div ref={container} id="map" className="w-full h-[500px] rounded-xl overflow-hidden shadow-lg"></div>
       {isLoading && (
         <div className="w-full h-full flex items-center justify-center bg-gray-100">
           <p className="text-gray-500">지도를 불러오는 중...</p>
         </div>
-      )} */}
+      )}
       <gmp-map
+        ref={container}
         center={`${center.lat},${center.lng}`}
         zoom={level}
         map-id="DEMO_MAP_ID"
@@ -53,6 +65,7 @@ function GoogleMap({ center, level = 3 }: GoogleMapProps) {
       <Script
         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=maps`}
         async
+        onLoad={init}
       />
     </div>
   );
